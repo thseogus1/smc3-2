1221a6776a73e5fb7a74848f1920bbef5143ee44
thseogus0110 <thseogus0110@naver.com>
2021-04-15 09:54:24 +0900

    0415 프로젝트 추가

---
+++ 0415/src/Number.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Number {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("璋濠 殮溘п ");
+		int a = sc.nextInt();
+		int b,c=0;
+		for(int i = 0; i < 10; i ++) {
+			b=(int)(Math.random()*50+1);
+			System.out.print(b+" ");
+			
+			if(a==b) {
+				c++;
+			}
+			
+		}
+		if(c>0) {
+			System.out.println("\n內六內六");
+		}
+		else System.out.println("\n統");
+	}
+}
